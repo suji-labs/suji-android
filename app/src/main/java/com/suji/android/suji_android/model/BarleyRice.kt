0cbece2e3413741b12073bfd16d6f16a2a550df1
KimBoWoon <haioer@naver.com>
2019-03-17 18:34:22 +0900

    i'm making a menu

---
+++ app/src/main/java/com/suji/android/suji_android/model/BarleyRice.kt
@@ -0,0 +1,3 @@
+package com.suji.android.suji_android.model
+
+data class BarleyRice(override val price: Int) : Food(price)
---
+++ app/src/main/java/com/suji/android/suji_android/model/Food.kt
@@ -0,0 +1,3 @@
+package com.suji.android.suji_android.model
+
+open class Food(open val price : Int)
